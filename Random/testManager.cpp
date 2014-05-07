c6e323313471b363daaed5d8e77a611dbb28d9d4
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-05-07 15:53:30 +0200

    majooooor progress

--- Random/testManager.cpp
+++ Random/testManager.cpp
@@ -8,27 +8,58 @@ enum class AtomState : int {Alive = 0, Unused = 1, Dead = 2};
 template<typename> class Manager;
 template<typename> class Handle;
 
-template<typename T> class Atom
+template<typename T> class Uncertain
 {
-	SSVU_ASSERT_STATIC(ssvu::isDefaultConstructible<T>(), "T must be default-constructible");
+	private:
+		ssvu::AlignedStorageBasic<T> storage;
 
+	public:
+		template<typename... TArgs> inline void init(TArgs&&... mArgs) noexcept(ssvu::isNothrowConstructible<T>())
+		{
+			new (&storage) T(std::forward<TArgs>(mArgs)...);
+		}
+		inline void deinit() noexcept(ssvu::isNothrowDestructible<T>()) { get().~T(); }
+
+		inline T& get() noexcept 				{ return reinterpret_cast<T&>(storage); }				
+		inline const T& get() const noexcept 	{ return reinterpret_cast<const T&>(storage); }				
+};
+
+template<typename T> class Atom 
+{
 	template<typename> friend class Manager;
 	template<typename> friend class Handle;
 
 	private:
 		Idx ctrlIdx;
 		AtomState state{AtomState::Unused};
-		T impl;
+		Uncertain<T> data;
 
-		template<typename... TArgs> inline void initImpl(TArgs&&... mArgs) 
-			noexcept(noexcept(T(std::forward<TArgs>(mArgs)...)))
+		// Initializes the internal data
+		template<typename... TArgs> inline void initData(TArgs&&... mArgs) noexcept(ssvu::isNothrowConstructible<T>())
 		{
-			impl = T(std::forward<TArgs>(mArgs)...);
+			SSVU_ASSERT(state == AtomState::Unused);
+			data.init(std::forward<TArgs>(mArgs)...);
+		}
+
+		// Deinitializes the internal data
+		inline void deinitData() noexcept(ssvu::isNothrowDestructible<T>())
+		{ 
+			SSVU_ASSERT(state != AtomState::Unused);
+			data.deinit();
 		}
 
 	public:
-		inline T& getImpl() noexcept 	{ return impl; }
-		inline void destroy() noexcept 	{ state = AtomState::Dead; }
+		inline Atom() = default;
+		inline Atom(Atom&&) = default;
+		inline Atom& operator=(Atom&&) = default;
+
+		inline T& getData() noexcept 				{ SSVU_ASSERT(state != AtomState::Unused); return data.get(); }				
+		inline const T& getData() const noexcept 	{ SSVU_ASSERT(state != AtomState::Unused); return data.get(); }	
+		inline void setDead() noexcept 				{ state = AtomState::Dead; }
+		
+		// Disallow copies
+		inline Atom(const Atom&) = delete;
+		inline Atom& operator=(const Atom&) = delete;
 };
 
 class Controller
@@ -52,10 +83,18 @@ template<typename T> class Handle
 
 		inline Handle(Manager<T>& mManager, Idx mCtrlIdx, Ctr mCtr) noexcept 
 			: manager(mManager), ctrlIdx{mCtrlIdx}, ctr{mCtr} { }
+
+		template<typename TT> inline TT getAtomImpl() noexcept
+		{
+			SSVU_ASSERT(isAlive());
+			return manager.getAtomFromController(manager.controllers[ctrlIdx]);
+		}
 		
 	public:
-		Atom<T>& getAtom() noexcept;
-		T& get() noexcept;
+		inline Atom<T>& getAtom() noexcept 				{ return getAtomImpl<Atom<T>&>(); }
+		inline const Atom<T>& getAtom() const noexcept 	{ return getAtomImpl<const Atom<T>&>(); }
+		inline T& get() noexcept						{ return getAtom().getData(); }
+		inline const T& get() const noexcept			{ return getAtom().getData(); }
 		bool isAlive() const noexcept;
 		void destroy() noexcept;
 };
@@ -69,9 +108,9 @@ template<typename T> class Manager
 		std::vector<Controller> controllers;
 		Idx size{0u};
 
-		inline void resizeStorage(std::size_t mOldSize, std::size_t mNewSize)
+		inline void growStorage(std::size_t mOldSize, std::size_t mNewSize)
 		{
-			SSVU_ASSERT(mNewSize >= 0);
+			SSVU_ASSERT(mNewSize >= 0 && mNewSize >= mOldSize);
 
 			atoms.resize(mNewSize);
 			controllers.resize(mNewSize);
@@ -84,21 +123,21 @@ template<typename T> class Manager
 			}
 		}
 
-		inline void resizeIfNeeded()
+		inline void growIfNeeded()
 		{
-			constexpr std::size_t resizeAmount{1};
+			constexpr std::size_t resizeAmount{10};
+
 			// If the first free index is valid, return
 			auto oldSize(atoms.size());
 			if(oldSize > size) return;
-			
 
-			// Calculate new size and resize storage
-			resizeStorage(oldSize, oldSize + resizeAmount);
+			// Calculate new size and grow storage
+			growStorage(oldSize, oldSize + resizeAmount);
 		}
 
 		inline void destroy(Idx mCtrlIdx) noexcept
 		{			
-			getAtomFromController(controllers[mCtrlIdx]).destroy();
+			getAtomFromController(controllers[mCtrlIdx]).setDead();
 		}
 
 		inline Controller& getControllerFromAtom(const Atom<T>& mAtom)
@@ -110,11 +149,23 @@ template<typename T> class Manager
 			return atoms[mController.idx];
 		}
 
+		inline void cleanUpMemory()
+		{
+			for(auto& a : atoms) 				
+				if(a.state != AtomState::Unused) 
+				{
+					a.deinitData();
+					a.state = AtomState::Unused;
+				}			
+		}
+
 	public:
-		inline Manager() { }
+		inline Manager() = default;
+		inline ~Manager() { cleanUpMemory(); }
 
 		inline void clear() noexcept
 		{
+			cleanUpMemory();
 			atoms.clear();
 			controllers.clear();
 			size = 0;
@@ -122,16 +173,16 @@ template<typename T> class Manager
 
 		inline void reserve(std::size_t mSize) 
 		{ 
-			resizeStorage(atoms.size(), mSize); 
+			growStorage(atoms.size(), mSize); 
 		}
 
 		template<typename... TArgs> inline Handle<T> createAtom(TArgs&&... mArgs)
 		{
 			// `size` may be greater than the sizes of the vectors - resize vectors if needed 
-			resizeIfNeeded();
+			growIfNeeded();
 
 			// `size` now is the first empty valid index - we create our atom there
-			atoms[size].initImpl(std::forward<TArgs>(mArgs)...);
+			atoms[size].initData(std::forward<TArgs>(mArgs)...);
 			atoms[size].state = AtomState::Alive;
 
 			// Update the controller
@@ -155,6 +206,7 @@ template<typename T> class Manager
 			auto i(atoms.size() - 1);			
 			for(; i > 0 && atoms[i].state == AtomState::Dead; --i)				
 			{
+				atoms[i].deinitData();
 				atoms[i].state = AtomState::Unused;
 				++(getControllerFromAtom(atoms[i]).ctr);				
 			}
@@ -167,9 +219,13 @@ template<typename T> class Manager
 			size = i;
 		}
 
-		template<typename TFunc> inline void forEach(const TFunc& mFunc)
+		template<typename TFunc> inline void forEach(TFunc mFunc)
 		{
-			for(auto i(0u); i < size; ++i) mFunc(atoms[i].impl);
+			for(auto i(0u); i < size; ++i) mFunc(atoms[i].getData());
+		}	
+		template<typename TFunc> inline void forEachAtom(TFunc mFunc)
+		{
+			for(auto i(0u); i < size; ++i) mFunc(atoms[i]);
 		}	
 
 		inline std::size_t getSize() const noexcept { return size; }
@@ -190,21 +246,10 @@ template<typename T> class Manager
 
 			ssvu::lo("ASTRS") << "\n";
 			std::size_t idx{0u};
-			for(const auto& a : atoms) std::cout << idx++ << ": " << a.impl << "\n";
+			//forEach([&idx](std::string& mS){ std::cout << idx++ << ": " << mS << "\n"; });
 			std::cout << std::endl;
 		}
 };
-
-template<typename T> inline Atom<T>& Handle<T>::getAtom() noexcept
-{ 
-	SSVU_ASSERT(isAlive());
-	return manager.getAtomFromController(manager.controllers[ctrlIdx]);
-}
-
-template<typename T> inline T& Handle<T>::get() noexcept
-{ 	
-	return getAtom().getImpl();
-}
 
 template<typename T> inline bool Handle<T>::isAlive() const noexcept
 { 
@@ -219,56 +264,114 @@ template<typename T> inline void Handle<T>::destroy() noexcept
 
 int main()
 {
-	Manager<std::string> test;
-
-	for(int k = 0; k < 2; ++k)
+	for(int j = 0; j < 2; ++j)
 	{
-		test.printState();
+		Manager<std::string> test;
 
-		auto a0 = test.createAtom();
-		auto a1 = test.createAtom();
-		auto a2 = test.createAtom();
-		auto a3 = test.createAtom();
-		auto a4 = test.createAtom();
-		auto a5 = test.createAtom();
-		auto a6 = test.createAtom();
+		for(int k = 0; k < 2; ++k)
+		{
+			test.printState();
 
-		a0.get() = "hi";
-		a4.get() = "ciao";
-		a6.get() = "bye";
+			auto a0 = test.createAtom();
+			auto a1 = test.createAtom();
+			auto a2 = test.createAtom();
+			auto a3 = test.createAtom();
+			auto a4 = test.createAtom();
+			auto a5 = test.createAtom();
+			auto a6 = test.createAtom();
 
-		test.printState();
+			a0.get() = "hi";
+			a4.get() = "ciao";
+			a6.get() = "bye";
 
-		a2.destroy();
-		a3.destroy();
-		a5.destroy();
-		//test.atoms[2].alive = false;
-		//test.atoms[3].alive = false;
-		//test.atoms[5].alive = false;
+			test.printState();
 
-		test.printState();
+			a2.destroy();
+			a3.destroy();
+			a5.destroy();
 
-		test.refresh();
+			test.printState();
 
-		test.printState();
+			test.refresh();
 
-		ssvu::lo("RESULT") << a0.get() << std::endl;
-		ssvu::lo("RESULT") << a4.get() << std::endl;
-		ssvu::lo("RESULT") << a6.get() << std::endl;
+			test.printState();
 
-		ssvu::lo("alive") << a0.isAlive() << std::endl;
-		ssvu::lo("alive") << a1.isAlive() << std::endl;
-		ssvu::lo("alive") << a2.isAlive() << std::endl;
-		ssvu::lo("alive") << a3.isAlive() << std::endl;
-		ssvu::lo("alive") << a4.isAlive() << std::endl;
-		ssvu::lo("alive") << a5.isAlive() << std::endl;
-		ssvu::lo("alive") << a6.isAlive() << std::endl;
+			ssvu::lo("RESULT") << a0.get() << std::endl;
+			ssvu::lo("RESULT") << a4.get() << std::endl;
+			ssvu::lo("RESULT") << a6.get() << std::endl;
 
-		test.forEach([](std::string& mStr){ mStr += "bb"; });
+			ssvu::lo("alive") << a0.isAlive() << std::endl;
+			ssvu::lo("alive") << a1.isAlive() << std::endl;
+			ssvu::lo("alive") << a2.isAlive() << std::endl;
+			ssvu::lo("alive") << a3.isAlive() << std::endl;
+			ssvu::lo("alive") << a4.isAlive() << std::endl;
+			ssvu::lo("alive") << a5.isAlive() << std::endl;
+			ssvu::lo("alive") << a6.isAlive() << std::endl;
 
-		ssvu::lo("RESULT2") << a0.get() << std::endl;
-		ssvu::lo("RESULT2") << a4.get() << std::endl;
-		ssvu::lo("RESULT2") << a6.get() << std::endl;
+			test.forEach([](std::string& mStr){ mStr += "bb"; });
+
+			ssvu::lo("RESULT2") << a0.get() << std::endl;
+			ssvu::lo("RESULT2") << a4.get() << std::endl;
+			ssvu::lo("RESULT2") << a6.get() << std::endl;
+		}		
+	}
+
+	struct TestRes 
+	{
+		std::string s;
+		TestRes(std::string mS) : s(std::move(mS)) { }
+	};
+
+	for(int j = 0; j < 2; ++j)
+	{
+		Manager<TestRes> test;
+
+		for(int k = 0; k < 2; ++k)
+		{
+			test.printState();
+
+			auto a0 = test.createAtom("atom00");
+			auto a1 = test.createAtom("atom01");
+			auto a2 = test.createAtom("atom02");
+			auto a3 = test.createAtom("atom03");
+			auto a4 = test.createAtom("atom04");
+			auto a5 = test.createAtom("atom05");
+			auto a6 = test.createAtom("atom06");
+
+			a0.get().s += " mod";
+			a4.get().s += " mod";
+			a6.get().s += " mod";
+
+			test.printState();
+
+			a2.destroy();
+			a3.destroy();
+			a5.destroy();
+
+			test.printState();
+
+			test.refresh();
+
+			test.printState();
+
+			ssvu::lo("RESULT") << a0.get().s << std::endl;
+			ssvu::lo("RESULT") << a4.get().s << std::endl;
+			ssvu::lo("RESULT") << a6.get().s << std::endl;
+
+			ssvu::lo("alive") << a0.isAlive() << std::endl;
+			ssvu::lo("alive") << a1.isAlive() << std::endl;
+			ssvu::lo("alive") << a2.isAlive() << std::endl;
+			ssvu::lo("alive") << a3.isAlive() << std::endl;
+			ssvu::lo("alive") << a4.isAlive() << std::endl;
+			ssvu::lo("alive") << a5.isAlive() << std::endl;
+			ssvu::lo("alive") << a6.isAlive() << std::endl;
+
+			test.forEach([](TestRes& mRes){ mRes.s += "bb"; });
+
+			ssvu::lo("RESULT2") << a0.get().s << std::endl;
+			ssvu::lo("RESULT2") << a4.get().s << std::endl;
+			ssvu::lo("RESULT2") << a6.get().s << std::endl;
+		}		
 	}
 	
 	return 0;

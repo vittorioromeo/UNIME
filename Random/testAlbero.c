5959a5570e933027e3b05cce88bb831164c3fbc6
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-06-03 12:33:45 +0200

    major progress

--- Random/testAlbero.c
+++ Random/testAlbero.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
 
-struct AlberoImpl;
+struct AlberoImpl; 
 typedef struct AlberoImpl Albero;
 
 struct AlberoImpl
@@ -13,6 +14,9 @@ struct AlberoImpl
 	int dato;
 };
 
+// Alloca memoria per un nodo, lo crea ed inizializza
+// Restituisce 1 in caso di errore
+// Il puntatore passato conterrà l'indirizzo del nodo creato
 int albero_Crea(Albero** mA, int mDato)
 {
 	*mA = (Albero*) malloc(1 * sizeof(Albero));
@@ -25,33 +29,52 @@ int albero_Crea(Albero** mA, int mDato)
 	return 0;
 }
 
+// Alloca memoria per un nodo, lo crea ed inizializza
+// Setta il padre del nodo creato ad mPadre
+// Restituisce 1 in caso di errore
 int albero_CreaFiglio(Albero** mA, Albero* mPadre, int mDato)
 {
+	assert(mPadre != NULL);
+
 	int risultato = albero_Crea(mA, mDato);
 	if(risultato == 0) (*mA)->px = mPadre;
 	
 	return risultato;
 }
 
+// Dealloca memoria per un nodo e setta il puntatore
+// passato a NULL
 void albero_Distruggi(Albero** mA)
 {
+	assert(*mA != NULL);
+
 	free(*mA);
 	*mA = NULL;
 }
 
+// Dealloca ricorsivamente memoria per un albero,
+// settando il puntatore a NULL
 void albero_Distruggi_Ricorsivo(Albero** mA)
 {
-	if((*mA)->sx != NULL) albero_Distruggi_Ricorsivo(&(*mA)->sx);
-	if((*mA)->dx != NULL) albero_Distruggi_Ricorsivo(&(*mA)->dx);
+	if(*mA == NULL) return;
 
+	albero_Distruggi_Ricorsivo(&(*mA)->sx);
+	albero_Distruggi_Ricorsivo(&(*mA)->dx);
 	albero_Distruggi(mA);
 }
 
+// Inserisce un nodo in maniera ordinata in un albero
+// Restituisce 1 in caso di errore
 int albero_Inserisci(Albero* mRadice, int mDato)
 {
-	Albero* prec = NULL;
+	Albero* prec;
 	Albero** p = &mRadice;
 
+	// Finchè non viene trovato un puntatore di un nodo
+	// esistente che punta ad un nodo NULL, p continua a 
+	// scendere, seguito da prec, nell'albero, mantenendo
+	// l'ordine dei dati
+	// Se viene trovato un dato già esistente, restituisce 1
 	while(*p != NULL)
 	{
 		prec = *p;		
@@ -63,104 +86,146 @@ int albero_Inserisci(Albero* mRadice, int mDato)
 	return albero_CreaFiglio(p, prec, mDato);
 }
 
+// Versione ricorsiva della funzione di sopra
 int albero_Inserisci_Ricorsivo(Albero* mRadice, int mDato)
 {
-	if(mDato < mRadice->dato) 
-	{
-		if(mRadice->sx == NULL) 
-			return albero_CreaFiglio(&mRadice->sx, mRadice, mDato);
-	
-		return albero_Inserisci_Ricorsivo(mRadice->sx, mDato);
-	}
-	else if(mDato > mRadice->dato) 
-	{
-		if(mRadice->dx == NULL) 
-			return albero_CreaFiglio(&mRadice->dx, mRadice, mDato);
-		
-		return albero_Inserisci_Ricorsivo(mRadice->dx, mDato);
-	}
+	if(mRadice == NULL) return albero_Crea(&mRadice, mDato);
 
-	return 1;
+	Albero** p = &mRadice->sx;
+
+	if(mDato == mRadice->dato) return 1;
+	if(mDato > mRadice->dato) p = &mRadice->dx; 
+	
+	if(*p == NULL) return albero_CreaFiglio(p, mRadice, mDato);
+	return albero_Inserisci_Ricorsivo(*p, mDato);
 }
 
+// Cerca un nodo di dato mDato, se lo trova restituisce un
+// puntatore ad esso, altrimenti restituisce NULL
 Albero* albero_Ricerca(Albero* mA, int mDato)
 {
+	assert(mA != NULL);
+
 	if(mA == NULL || mDato == mA->dato) return mA;
 	return albero_Ricerca(mDato < mA->dato ? mA->sx : mA->dx, mDato);
 }
 
+// Scende continuamente a sinistra, restituendo il puntatore al
+// nodo di valore minimo o NULL
 Albero* albero_getMinimo(Albero* mA)
 {
 	while(mA->sx != NULL) mA = mA->sx;
 	return mA;
 }
 
+// Scende continuamente a destra, restituendo il puntatore al
+// nodo di valore massimo o NULL
 Albero* albero_getMassimo(Albero* mA)
 {
 	while(mA->dx != NULL) mA = mA->dx;
 	return mA;
 }
 
+Albero* albero_getFiglioSx(Albero* mA) { assert(mA != NULL); return mA->sx; }
+Albero* albero_getFiglioDx(Albero* mA) { assert(mA != NULL); return mA->dx; }
+
+typedef Albero*(*FPAl)(Albero*);
+
+Albero* albero_getSuccPred(Albero* mA, FPAl mFnMinMax, FPAl mFnF1, FPAl mFnF2) 
+{
+	assert(mA != NULL);
+
+	// Se ha un figlio F1, restituisce il min/max tra 
+	// i maggioranti/minoranti
+	Albero* figlioF1 = (*mFnF1)(mA);
+	if(figlioF1 != NULL) return (*mFnMinMax)(figlioF1);
+
+	// Se non ha figlio F1 e non ha padre, restituisce NULL
+	if(mA->px == NULL) return NULL;
+
+	// Se il nodo passato è il figlio F2 di suo padre,
+	// suo padre è il successore - restituiscilo
+	if(mA == (*mFnF2)(mA->px)) return mA->px;
+
+	// Altrimenti risale l'albero finchè non trova un nodo
+	// che è il figlio F2 di suo padre - se non esiste
+	// restituisce NULL
+	while(mA->px != NULL && mA != (*mFnF2)(mA->px)) mA = mA->px;
+	return mA->px;
+}
+
 Albero* albero_getSuccessore(Albero* mA)
 {
-	if(mA->dx != NULL) return albero_getMinimo(mA->dx);
-	if(mA->px == NULL) return NULL;
-	if(mA == mA->px->sx) return mA->px;
-
-	while(mA->px != NULL && mA != mA->px->sx) mA = mA->px;
-	return mA->px;
+	return albero_getSuccPred(mA, &albero_getMinimo,
+		&albero_getFiglioDx, &albero_getFiglioSx);
 }
 
 Albero* albero_getPredecessore(Albero* mA)
 {
-	if(mA->sx != NULL) return albero_getMassimo(mA->sx);
-	if(mA->px == NULL) return NULL;
-	if(mA == mA->px->dx) return mA->px;
-
-	while(mA->px != NULL && mA != mA->px->dx) mA = mA->px;
-	return mA->px;
+	return albero_getSuccPred(mA, &albero_getMassimo,
+		&albero_getFiglioSx, &albero_getFiglioDx);
 }
 
-int albero_Rimuovi(Albero* mA)
+// Rimuove un nodo da un albero, assume che il nodo faccia parte di un albero
+void albero_Rimuovi(Albero* mA)
 {
+	assert(mA != NULL);
+
 	Albero* padre = mA->px;
 
+	// Se il nodo da rimuovere ha un padre
 	if(padre != NULL)
 	{
+		// Se mA è il figlio sx del padre, figlioPadre punterà all'indirizzo
+		// del figlio sx del padre di mA, altrimenti figlioPadre punterà
+		// all'indirizzo del figlio dx del padre di mA
 		Albero** figlioPadre = (padre->sx == mA) ? &(padre->sx) : &(padre->dx);
 	
+		// Se mA non ha figli, *figlioPadre sarà settato a NULL
+		// Se mA ha solo figlio sx, *figlioPadre sarà settato al figlio sx
+		// Se mA ha solo figlio dx, *figlioPadre sarà settato al figlio dx
 		if(mA->sx == NULL && mA->dx == NULL) *figlioPadre = NULL;
 		else if(mA->sx != NULL && mA->dx == NULL) *figlioPadre = mA->sx;
 		else if(mA->sx == NULL && mA->dx != NULL) *figlioPadre = mA->dx;
 	}
 
+	// Altrimenti, se il padre non esiste (cioè mA è la radice), oppure
+	// mA ha due figli
 	if(padre == NULL || (mA->sx != NULL && mA->dx != NULL))
 	{
+		// Troviamo o il predecessore o il successore di mA
 		Albero* next = albero_getSuccessore(mA);
 		if(next == NULL) next = albero_getPredecessore(mA);
+
+		// Settiamo il dato contenuto in mA con il dato contenuto
+		// nel suo successore/predecessore
 		mA->dato = next->dato;
 		
-		return albero_Rimuovi(next);
+		// Rimuoviamo ricorsivamente sul successore/predecessore
+		albero_Rimuovi(next);
+		return;
 	}
 
+	// Deallochiamo la memoria per il nodo cancellato
 	albero_Distruggi(&mA);
-	return 0;
 }
 
+// Restituisce l'altezza di un albero
 int albero_getAltezza(Albero* mRadice)
 {
+	// Se l'albero è vuoto (senza radice), restituisce -1
 	if(mRadice == NULL) return -1;
 
+	// Altrimenti restituisce l'altezza maggiore tra il sottoalbero
+	// sx o dx, calcolandola ricorsivamente
 	int hSx = albero_getAltezza(mRadice->sx);
 	int hDx = albero_getAltezza(mRadice->dx);
-
 	return hSx > hDx ? hSx + 1 : hDx + 1;
 }
 
 void stampaX(int mSpazi, const char* mStr)
 {
-	int i;
-	for(i = 0; i < mSpazi; ++i) printf("%s", mStr);
+	for(; mSpazi >= 0 ; --mSpazi) printf("%s", mStr);
 }
 
 int contaCifre(int mNumero)
@@ -216,8 +281,7 @@ void albero_AttraversaLiv(Albero* mRadice)
 			stampaX(spazi - 1, " ");
 			printf("|");
 			
-			int j;
-			for(j = 0; j < passaggi - 1; ++j)
+			for(; passaggi > 1; --passaggi) 
 			{
 				stampaX(precSpazi - 1, " ");
 				printf("|");
@@ -263,3 +327,36 @@ int main()
 	
  	return 0;
 }
+
+/*
+Albero* albero_getSuccessore(Albero* mA)
+{
+	// Se ha un figlio destro, restituisce il minore tra 
+	// i maggioranti
+	if(mA->dx != NULL) return albero_getMinimo(mA->dx);
+	
+	// Se non ha figlio destro e non ha padre, restituisce
+	// NULL
+	if(mA->px == NULL) return NULL;
+	
+	// Se il nodo passato è il figlio sinistro di suo padre,
+	// suo padre è il successore - restituiscilo
+	if(mA == mA->px->sx) return mA->px;
+
+	// Altrimenti risale l'albero finchè non trova un nodo
+	// che è il figlio sinistro di suo padre - se non esiste
+	// restituisce NULL
+	while(mA->px != NULL && mA != mA->px->sx) mA = mA->px;
+	return mA->px;
+}
+
+Albero* albero_getPredecessore(Albero* mA)
+{
+	if(mA->sx != NULL) return albero_getMassimo(mA->sx);
+	if(mA->px == NULL) return NULL;
+	if(mA == mA->px->dx) return mA->px;
+
+	while(mA->px != NULL && mA != mA->px->dx) mA = mA->px;
+	return mA->px;
+}
+*/
